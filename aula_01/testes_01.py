4bf6eb4bdf2899b6ec3e4fdc2475d28db08076b0
Carlos Cabral <elaborata@cabr.al>
2025-05-20 19:00:40 -0300

    Here be dragons

---
+++ aula_01/testes_01.py
@@ -0,0 +1,61 @@
+# codigo_turma = 13317
+# nome_professor = "Carlos"
+# alunos_presencial = 9
+# # Mostra o dobro dos alunos presenciais
+# print(alunos_presencial * 2)
+
+# variavel =  # Erro
+
+
+# bruto = 1000
+# imposto = 100
+# liquido = bruto - imposto
+# print(liquido)
+
+
+# variavel_inteiro = 400  # 400 -> Int
+# variavel_decimal = 1.99  # 1.99 -> Float
+# variavel_texto = "Vovó foi à feira"  # Vovó foi à feira -> Str
+# variavel_verdadeiro = True  # True -> Bool
+# variavel_falso = False  # False -> Bool
+# variavel_nulo = None  # None -> Null
+
+
+# a = 10  # Int
+# b = "10"  # Str
+# c = a + b  # Erro de Tipo
+# print(c)
+
+
+# a = 10  # Int
+# b = "10"  # Str
+# c = a + int(b)  # Soma de inteiros
+# print(c)  # 20 -> Int
+
+
+# a = 10  # Int
+# b = "Batman"  # Str
+# c = a + int(b)  # Erro de Valor
+# print(c)
+
+
+# a = 10  # Int
+# b = "10"  # Str
+# c = str(a) + b  # Soma de Strings, Concatenação de String
+# print(c)  # 1010 -> Str
+
+# print("Batman" + "Robin")  # BatmanRobin -> Str
+
+
+# conversao_implicita = 6 / 3
+# print(conversao_implicita)
+
+
+# nome_usuario = "João"
+# print(nome_usuario)
+# pontuacao_jogador = 6000
+# print("Olá, Mundo!")
+
+# print(4500, 4000, 3000, False, "Abacate")
+clima = "nublado"
+print("Hoje está", clima, "Tenha um bom dia.")
